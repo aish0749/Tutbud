ad57f5d44f26760cb46c0179646a29ce74cea58d
aish0749 <aishwaryasaxena57@gmail.com>
2023-12-02 12:10:19 +0530

    modify urls and get dynamic values

--- base/views.py
+++ base/views.py
@@ -9,10 +9,15 @@ rooms = [
     {'id': 3, 'name': 'frontend developers'},
 ]
 
-context = {'rooms': rooms}
 def home(request):
-    return render(request, 'home.html', context)
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
-def room(request):
-    return render(request, 'room.html', context)
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
 
