da00e31f51bf8d5a9ff1cb4cab6f47f7b87276e2
aish0749 <aishwaryasaxena57@gmail.com>
2023-12-06 17:42:54 +0530

    add create and update to room.

--- base/urls.py
+++ base/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('room/<str:pk>/', views.room, name="room"),
-    path('create_room/', views.create_room, name="create-room")
+    path('create_room/', views.create_room, name="create-room"),
+    path('update_room/<str:pk>/', views.update_room, name="update-room"),    
 ] 
--- base/views.py
+++ base/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Room
 from .forms import RoomForm
 
@@ -16,5 +16,18 @@ def room(request, pk):
 
 def create_room(request):
     form = RoomForm()
-    context={'form': form}
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+
+def update_room(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    context = {'form': form}
     return render(request, 'base/room_form.html', context)
