1553378c24a36331802e279480481c9666226d0f
aish0749 <aishwaryasaxena57@gmail.com>
2023-12-07 14:51:30 +0530

    add delete function and complete the crud operations.

--- base/urls.py
+++ base/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('room/<str:pk>/', views.room, name="room"),
     path('create_room/', views.create_room, name="create-room"),
     path('update_room/<str:pk>/', views.update_room, name="update-room"),    
+    path('delete_room/<str:pk>/', views.delete_room, name="delete-room"),
 ] 
